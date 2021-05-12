c61dd27c8b579590d35ec5b2ce120c99a2a3844e
Charlotte <rossetti@evolbio.mpg.de>
2021-05-12 17:29:17 +0200

    getting session with high payoffs in both game ready

--- multi_prisoner/pages.py
+++ multi_prisoner/pages.py
@@ -140,7 +140,7 @@ class Results(Page):
             return True
 
     timer_text = 'You are about to be automatically moved to the next round decision page'
-    timeout_seconds = 2 * 6000
+    timeout_seconds = 2 * 60
     # my_page_timeout_seconds = 90
     #
     # def get_timeout_seconds(self):
